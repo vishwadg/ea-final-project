85f84f3e6724d963232dec0bbb8bda1ee80be104
Sushil Subedi <sushil173@gmail.com>
2022-11-22 16:57:57 -0600

    add dependencies on comment service

--- comment-service/src/main/java/edu/miu/commentservice/CommentServiceApplication.java
+++ comment-service/src/main/java/edu/miu/commentservice/CommentServiceApplication.java
@@ -3,9 +3,8 @@ package edu.miu.commentservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class CommentServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CommentServiceApplication.class, args);
     }
