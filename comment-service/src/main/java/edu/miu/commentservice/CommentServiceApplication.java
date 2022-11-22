2131f25e1cbe94ad33cb1784ea16c57d779ef33d
Sushil Subedi <sushil173@gmail.com>
2022-11-22 14:00:05 -0600

    wip on comment and config service

--- comment-service/src/main/java/edu/miu/commentservice/CommentServiceApplication.java
+++ comment-service/src/main/java/edu/miu/commentservice/CommentServiceApplication.java
@@ -3,11 +3,11 @@ package edu.miu.commentservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class CommentServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CommentServiceApplication.class, args);
     }
-
+    
 }
---
+++ comment-service/src/main/java/edu/miu/commentservice/config/WebSecurityConfig.java
@@ -0,0 +1,14 @@
+package edu.miu.commentservice.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+}
