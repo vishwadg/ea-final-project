4fab88b81f00b01d4f8ffc36c40cf44bfd92661f
abhay-rawal <avyrwl1@gmail.com>
2023-02-06 11:22:51 -0600

    Avatar Service Crud Completed

--- avatar-service/src/main/java/miu/edu/AvatarServiceApplication.java
+++ avatar-service/src/main/java/miu/edu/AvatarServiceApplication.java
@@ -1,11 +1,12 @@
 package miu.edu;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AvatarServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(AvatarServiceApplication.class, args);
+     SpringApplication.run(AvatarServiceApplication.class, args);
     }
 }
